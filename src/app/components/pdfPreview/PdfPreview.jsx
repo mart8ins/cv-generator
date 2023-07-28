0772fe78119c60785e3df8dd1090a15611bbc6cc
mart8ins <mart8ins@gmail.com>
2023-07-28 20:13:08 +0300

    implemented html2pdf

--- src/app/components/pdfPreview/PdfPreview.jsx
+++ src/app/components/pdfPreview/PdfPreview.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import DetailsPreview from "./components/DetailsPreview";
 import { AppThemeContext } from "@/app/context/theme-context";
 import ExpierencePreview from "./components/ExpierencePreview";
@@ -6,13 +6,31 @@ import EducationPreview from "./components/EducationPreview";
 import ProjectsPreview from "./components/ProjectsPreview";
 import SkillsPreview from "./components/SkillsPreview";
 import PDFoptions from "./components/PdfOptions";
+import html2pdf from "html2pdf.js";
 
 export default function PdfPreview() {
-    const { theme, pdfScale } = useContext(AppThemeContext);
+    const { theme, pdfScale, setPdfScale } = useContext(AppThemeContext);
+    const pdfRef = useRef(null);
+
+    const handleGeneratePDF = () => {
+        setPdfScale(0.8);
+        const element = pdfRef.current;
+        const opt = {
+            margin: 10,
+            filename: "cv.pdf",
+            image: { type: "jpeg", quality: 1 },
+            html2canvas: { scale: 1 },
+            jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+        };
+
+        html2pdf().from(element).set(opt).save();
+    };
+
     return (
         <div className="pdfPreview">
-            <PDFoptions/>
+            <PDFoptions handleGeneratePDF={handleGeneratePDF} />
             <div
+                ref={pdfRef}
                 className="pdf-document"
                 style={{ borderTopColor: theme.color, width: "793.33px", height: "1122.67px", transform: `scale(${pdfScale})` }}>
                 <div className="preview-group">
@@ -28,11 +46,11 @@ export default function PdfPreview() {
                 </div>
 
                 <div className="preview-group">
-                    <ProjectsPreview/>
+                    <ProjectsPreview />
                 </div>
 
                 <div className="preview-group">
-                    <SkillsPreview/>
+                    <SkillsPreview />
                 </div>
             </div>
         </div>
--- src/app/components/pdfPreview/components/PdfOptions.jsx
+++ src/app/components/pdfPreview/components/PdfOptions.jsx
@@ -3,8 +3,10 @@ import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
 import { Slider } from "@mui/material";
 import { useContext } from "react";
 
-export default function PDFoptions() {
+
+export default function PDFoptions({handleGeneratePDF}) {
     const { pdfScale, setPdfScale } = useContext(AppThemeContext);
+
     return (
         <div className="pdf-document-options">
             <div>
@@ -16,12 +18,12 @@ export default function PDFoptions() {
                     marks
                     min={0.5}
                     max={1.5}
-                    onChange={(e: any) => setPdfScale(e.target.value)}
+                    onChange={(e) => setPdfScale(e.target.value)}
                     value={pdfScale}
                     style={{ color: "white" }}
                 />
             </div>
-            <div className="pdf-save-container">
+            <div onClick={()=> handleGeneratePDF()} className="pdf-save-container">
                 <div>Save</div>
                 <PictureAsPdfIcon />
             </div>
